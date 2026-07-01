42f6244879f68c1ef007b4c9024e67afe435aaae
Patrik Ranudd <patrik.ranudd@sogeti.se>
2026-07-01 19:54:10 +0200

    Added convertShapeToPath to the svgo config

--- dump.svgo.mjs
+++ dump.svgo.mjs
@@ -20,6 +20,7 @@ export default {
     'moveElemsAttrsToGroup',
     'collapseGroups',
     'moveGroupAttrsToElems',
+    'convertShapeToPath',
     'convertTransform',
     {
       name: 'convertPathData',
